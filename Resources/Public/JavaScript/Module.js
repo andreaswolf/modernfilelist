a4d8ae7fc8aced4173814abeec12f3dd72bd6226
Andreas Wolf <dev@a-w.io>
2016-02-25 14:36:27 +0100

    [FEATURE] Display file size with proper units

--- Resources/Public/JavaScript/Module.js
+++ Resources/Public/JavaScript/Module.js
@@ -5,7 +5,24 @@ define(['TYPO3/CMS/Modernfilelist/FileService', 'TYPO3/CMS/Modernfilelist/FileLi
 		var mod = angular.module('filelistApp', ['angularUtils.directives.dirPagination']);
 
 		mod.controller('FileListController', FileListController)
-			.factory('FileService', FileService);
+			.factory('FileService', FileService)
+			// https://gist.github.com/thomseddon/3511330
+			.filter('bytes', function() {
+				return function(bytes, precision) {
+					if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) {
+						return '-';
+					}
+					if (typeof precision === 'undefined') {
+						precision = 1;
+					}
+					if (bytes === 0) {
+						return '0 B';
+					}
+					var units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB'],
+						number = Math.floor(Math.log(bytes) / Math.log(1024));
+					return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+				}
+			});
 
 		return mod;
 	});
