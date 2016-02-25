f2b2ca599b1b1bd37f9523da751177967f4a5e93
Andreas Wolf <dev@a-w.io>
2016-02-25 15:15:21 +0100

    [FEATURE] Filter files by parts of the name

--- Resources/Public/JavaScript/FileListController.js
+++ Resources/Public/JavaScript/FileListController.js
@@ -3,6 +3,7 @@ define(function () {
 	return ['$attrs', '$scope', 'FileService', function($attrs, $scope, FileService) {
 
 		$scope.showOnlyWithoutReferences = false;
+		$scope.nameFilter = "";
 
 		var that = this;
 		FileService.getFiles($attrs['storage'], $attrs['path']).success(function(data) {
@@ -11,7 +12,8 @@ define(function () {
 
 		$scope.filterFileList = function() {
 			return function (file) {
-				return (!$scope.showOnlyWithoutReferences || file['_referenceCount'] == 0);
+				return (!$scope.showOnlyWithoutReferences || file['_referenceCount'] == 0) &&
+					($scope.nameFilter == "" || file['name'].toLowerCase().indexOf($scope.nameFilter.toLowerCase()) != -1);
 			};
 		}
 
