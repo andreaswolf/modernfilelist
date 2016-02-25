02d12f80d5ebba997203deac9367d44bb937273e
Andreas Wolf <dev@a-w.io>
2016-02-25 15:06:50 +0100

    [FEATURE] Filter file list by # of references

--- Resources/Public/JavaScript/FileListController.js
+++ Resources/Public/JavaScript/FileListController.js
@@ -1,11 +1,19 @@
 'use strict';
 define(function () {
-	return ['$attrs', 'FileService', function($attrs, FileService) {
+	return ['$attrs', '$scope', 'FileService', function($attrs, $scope, FileService) {
+
+		$scope.showOnlyWithoutReferences = false;
+
 		var that = this;
 		FileService.getFiles($attrs['storage'], $attrs['path']).success(function(data) {
-			console.debug(data);
 			that.files = data['files'];
 		});
+
+		$scope.filterFileList = function() {
+			return function (file) {
+				return (!$scope.showOnlyWithoutReferences || file['_referenceCount'] == 0);
+			};
+		}
 
 	}];
 });
